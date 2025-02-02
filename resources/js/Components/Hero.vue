36b5e31d84c6268995f0ef38d737ac6bf68a8c10
perkimbjm <ampihkumuh@gmail.com>
2025-02-03 00:40:05 +0800

    Add Image Hover in Hero Landingpage

--- resources/js/Components/Hero.vue
+++ resources/js/Components/Hero.vue
@@ -1,56 +1,44 @@
 <template>
     <section id="hero" class="w-screen h-[90vh] sm:h-screen sm:mb-4 mt-8">
-        <div class="container mx-auto px-4 max-w-6xl">
-            <div
-                class="flex flex-col md:flex-row md:items-center md:justify-between"
-            >
-                <div
-                    class="col-lg-5 pt-2 mb-10 pt-lg-0 order-2 md:order-1 lg:order-1 xl:order-1 2xl:order-1 flex justify-center"
-                    id="text-hero"
-                >
-                    <div
-                        data-aos="zoom-out"
-                        data-aos-delay="400"
-                        class="text-center lg:text-left mt-1 p-3 text-hero"
-                    >
-                        <h1 class="text-4xl font-semibold font-lato py-3">
+        <div class="container max-w-6xl px-4 mx-auto">
+            <div class="flex flex-col md:flex-row md:items-center md:justify-between">
+                <div class="flex justify-center order-2 pt-2 mb-10 col-lg-5 pt-lg-0 md:order-1 lg:order-1 xl:order-1 2xl:order-1"
+                    id="text-hero">
+                    <div data-aos="zoom-out" data-aos-delay="400" class="p-3 mt-1 text-center lg:text-left text-hero">
+                        <h1 class="py-3 text-4xl font-semibold font-lato">
                             SIBEDAH SERU
                         </h1>
                         <h3 class="text-3xl font-lato">SISTEM INFORMASI</h3>
                         <h3 class="text-3xl font-lato">BEDAH SERIBU RUMAH</h3>
-                        <h4 class="text-xl mt-2 font-lato">
+                        <h4 class="mt-2 text-xl font-lato">
                             BID. PERUMAHAN RAKYAT & KAWASAN PERMUKIMAN
                         </h4>
                         <h4 class="text-xl font-lato">KABUPATEN BALANGAN</h4>
-                        <div class="text-center lg:text-left mb-10">
-                            <a
-                                href="#cta"
-                                class="box-border appearance-none bg-[#f8f550] border-2 border-[#f8f550] text-[#292928] cursor-pointer self-center text-base font-semibold leading-none text-center uppercase font-montserrat hover:bg-[#ffc802] hover:text-[#1a1919] focus:outline-nonefirst scrollto rounded-full my-3 px-4 py-3 inline-block"
-                                >JELAJAHI SEKARANG →</a
-                            >
+                        <div class="mb-10 text-center lg:text-left">
+                            <a href="#cta"
+                                class="box-border appearance-none bg-[#f8f550] border-2 border-[#f8f550] text-[#292928] cursor-pointer self-center text-base font-semibold leading-none text-center uppercase font-montserrat hover:bg-[#ffc802] hover:text-[#1a1919] focus:outline-nonefirst scrollto rounded-full my-3 px-4 py-3 inline-block">JELAJAHI
+                                SEKARANG →</a>
                         </div>
                     </div>
                 </div>
-                <div
-                    class="col-12 col-lg-7 order-1 md:order-2 lg:order-2 xl:order-2 2xl:order-2 flex items-center"
-                    id="img-hero"
-                >
-                    <img
-                        src="/img/hero_c.webp"
-                        class="img-fluid animated hero-img"
-                        alt="bedah-seribu-rumah"
-                        data-aos="zoom-out"
-                        data-aos-delay="600"
-                        width="629" 
-                        height="512"
-                        loading="eager"
-                    />
+                <div class="flex items-center order-1 col-12 col-lg-7 md:order-2 lg:order-2 xl:order-2 2xl:order-2"
+                    id="img-hero">
+                    <img :src="isHovered ? '/img/hero_h.webp' : '/img/hero_c.webp'"
+                        class="transition-opacity duration-300 ease-in-out img-fluid animated hero-img"
+                        alt="bedah-seribu-rumah" data-aos="zoom-out" data-aos-delay="600" width="629" height="512"
+                        loading="eager" @mouseenter="isHovered = true" @mouseleave="isHovered = false" />
                 </div>
             </div>
         </div>
     </section>
     <!-- End Hero -->
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isHovered = ref(false)
+</script>
 
 <style scoped>
 .text-hero h1 {
@@ -65,5 +53,9 @@
 .text-hero h2,
 h3 {
     font-family: "Roboto", sans-serif;
+}
+
+.hero-img {
+    transition: all 0.3s ease-in-out;
 }
 </style>
