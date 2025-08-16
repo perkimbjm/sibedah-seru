9a699ac14c5291e34efe867e1445da8898ac0206
perkimbjm <ampihkumuh@gmail.com>
2025-08-16 13:41:35 +0800

    ubah panduan

--- resources/js/Pages/Guide.vue
+++ resources/js/Pages/Guide.vue
@@ -1,9 +1,8 @@
 <template>
     <MainLayout>
         <div class="w-full">
-            <iframe
-                src="https://docs.google.com/presentation/d/e/2PACX-1vSGwJyxcExpeI-3Sm-XZbRlA2aZ7IwX2A9nfKh3glksPlRJPp_crxEz2wOIJg_Jy7z2i9I2l5Em4XMq/embed?start=true&loop=false&delayms=7000"
-                frameborder="0" allowfullscreen="true" class="w-full h-[88vh]"></iframe>
+            <iframe src="https://sibedahseru.web.id/storage/doc/BUKU%20PANDUAN%20Sibedah%20Seru.pdf" frameborder="0"
+                allowfullscreen="true" class="w-full h-[88vh]"></iframe>
         </div>
         <FooterBar :links="links" />
     </MainLayout>
